f52b6ce9db3879f7ec97430498a8ad0bd1c3b46e
Julia Wang <90004776+jlwang03@users.noreply.github.com>
2022-03-26 14:19:08 -0700

    modified code to interpret from one sensor - works!

--- JJS/JJS.ino
+++ JJS/JJS.ino
@@ -1,82 +1,37 @@
-/*
- * Rui Santos
- * Complete Project Details http://randomnerdtutorials.com
-*/
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com  
+  Based on the Dallas Temperature Library example
+*********/
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 // Data wire is conntec to the Arduino digital pin 4
-const int ONE_WIRE_BUS = 4; 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+#define ONE_WIRE_BUS 4
+
+// Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature sensor 
 DallasTemperature sensors(&oneWire);
 
-int numberOfDevices; // Number of temperature devices found
-
-DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address
-
-void setup(void) {
-  // start serial port
+void setup(void)
+{
+  // Start serial communication for debugging purposes
   Serial.begin(9600);
-  
   // Start up the library
   sensors.begin();
-  
-  // Grab a count of devices on the wire
-  numberOfDevices = sensors.getDeviceCount();
-  
-  // locate devices on the bus
-  Serial.print("Locating devices...");
-  Serial.print("Found ");
-  Serial.print(numberOfDevices, DEC);
-  Serial.println(" devices.");
-
-  // Loop through each device, print out address
-  for(int i=0;i<numberOfDevices; i++) {
-    // Search the wire for address
-    if(sensors.getAddress(tempDeviceAddress, i)) {
-      Serial.print("Found device ");
-      Serial.print(i, DEC);
-      Serial.print(" with address: ");
-      printAddress(tempDeviceAddress);
-      Serial.println();
-    } else {
-      Serial.print("Found ghost device at ");
-      Serial.print(i, DEC);
-      Serial.print(" but could not detect address. Check power and cabling");
-    }
-  }
 }
 
-void loop(void) { 
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  // Loop through each device, print out temperature data
-  for(int i=0;i<numberOfDevices; i++) {
-    // Search the wire for address
-    if(sensors.getAddress(tempDeviceAddress, i)){
-    
-    // Output the device ID
-    Serial.print("Temperature for device: ");
-    Serial.println(i,DEC);
-
-    // Print the data
-    float tempC = sensors.getTempC(tempDeviceAddress);
-    Serial.print("Temp C: ");
-    Serial.print(tempC);
-    Serial.print(" Temp F: ");
-    Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
-    }   
-  }
-  delay(5000);
-}
-
-// function to print a device address
-void printAddress(DeviceAddress deviceAddress) {
-  for (uint8_t i = 0; i < 8; i++) {
-    if (deviceAddress[i] < 16) Serial.print("0");
-      Serial.print(deviceAddress[i], HEX);
-  }
+void loop(void){ 
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  
+  Serial.print("Celsius temperature: ");
+  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  Serial.print(sensors.getTempCByIndex(0)); 
+  Serial.print(" - Fahrenheit temperature: ");
+  Serial.println(sensors.getTempFByIndex(0));
+  delay(1000);
 }
