ef072f3491f0c0219f416c320d99b4d53c04d921
Julia Wang <90004776+jlwang03@users.noreply.github.com>
2022-03-26 12:30:27 -0700

    added folder for arduino programming

---
+++ JJS/JJS.ino
@@ -0,0 +1,13 @@
+const int buttonPin;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(/**/, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
