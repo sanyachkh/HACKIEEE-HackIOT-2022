e6c5fdc07937d26cc688cb309fade8234e2e470a
Sanya Khattar <sckhatta@usc.edu>
2022-03-26 18:25:26 -0700

    gps.ino with updated formate from GPSPlus added

--- gps/gps.ino
+++ gps/gps.ino
@@ -2,10 +2,15 @@
  * where the areas with fire inducing conditions,
  * or fires, are
  */
-
+#include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
 static const int RXPin = 4, TXPin = 3;
-static const uint32_t GPSBaud = 9600;
+static const uint32_t GPSBaud = 4800; // had to change from 9600 to 4800 for
+//format correction
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
 void setup()
@@ -16,8 +21,66 @@ void setup()
 void loop()
 {
   // Output raw GPS data to the serial monitor
-  while (ss.available() > 0){
-    Serial.write(ss.read());
+  while (ss.available() > 0)
+  {
+      if (gps.encode(ss.read()))
+      displayInfo();
+  }
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
   }
 }
-//hi
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
